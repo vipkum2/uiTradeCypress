568d87e2e8531640ca2863383a8b1d774a7611c1
Vipin Kumar <jobsforvipinkumar@gmail.com>
2025-12-07 21:40:17 +0530

    deleted

--- cypress/support/commands.js
+++
@@ -1,8 +0,0 @@
-/***********************************************************
- * This script part of Cypress framework.
- * We can add common commands/functions here.
- ***********************************************************/
-
-import "@testing-library/cypress/add-commands";
-
-//we can add custom commands here
